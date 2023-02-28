c223bde803d8d379bfed9e3715e471d7d4c19d99
mohandayman <66260018+mohandayman@users.noreply.github.com>
2023-02-28 14:00:10 +0200

    when we send massege the the number of user increased

---
+++ Connect 4 Project/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Connect_4_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.send_button = new System.Windows.Forms.Button();
+            this.New_tab = new System.Windows.Forms.Button();
+            this.massegeBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // send_button
+            // 
+            this.send_button.Location = new System.Drawing.Point(528, 349);
+            this.send_button.Name = "send_button";
+            this.send_button.Size = new System.Drawing.Size(153, 63);
+            this.send_button.TabIndex = 1;
+            this.send_button.Text = "send";
+            this.send_button.UseVisualStyleBackColor = true;
+            this.send_button.Click += new System.EventHandler(this.send_button_Click);
+            // 
+            // New_tab
+            // 
+            this.New_tab.Location = new System.Drawing.Point(89, 358);
+            this.New_tab.Name = "New_tab";
+            this.New_tab.Size = new System.Drawing.Size(153, 54);
+            this.New_tab.TabIndex = 2;
+            this.New_tab.Text = "new Tab";
+            this.New_tab.UseVisualStyleBackColor = true;
+            this.New_tab.Click += new System.EventHandler(this.New_tab_Click);
+            // 
+            // massegeBox
+            // 
+            this.massegeBox.Location = new System.Drawing.Point(289, 180);
+            this.massegeBox.Multiline = true;
+            this.massegeBox.Name = "massegeBox";
+            this.massegeBox.Size = new System.Drawing.Size(188, 144);
+            this.massegeBox.TabIndex = 3;
+            this.massegeBox.TextChanged += new System.EventHandler(this.massegeBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 450);
+            this.Controls.Add(this.massegeBox);
+            this.Controls.Add(this.New_tab);
+            this.Controls.Add(this.send_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button send_button;
+        private System.Windows.Forms.Button New_tab;
+        private System.Windows.Forms.TextBox massegeBox;
+    }
+}
+
---
+++ Connect 4 Project/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect_4_Project
+{
+
+   
+    public partial class Form1 : Form
+    {
+        client Game_client;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            Game_client = new client();
+           
+
+        }
+
+       
+
+        private void send_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               Game_client.Send_Massege(this.massegeBox.Text);
+
+            }
+
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
+
+           
+        }
+
+       
+
+        private void New_tab_Click(object sender, EventArgs e)
+        {
+            Thread newTab_thread = new Thread(Game_client.open_tab);
+            newTab_thread.Start();
+        }
+
+        private void massegeBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+
+    public class client
+    {
+        TcpClient tcp_client_player;
+        IPAddress severIP;
+        int portNumber;
+        Socket client_socket;
+        NetworkStream client_network_stream;
+        StreamWriter client_stream_writer;
+        StreamReader client_stream_reader;
+        static int Connected_players;
+
+
+        static client()
+        {
+            Connected_players = 0;
+
+        }
+        public client()
+        {
+            Connected_players++;
+            create_Client("connected" + Connected_players.ToString());
+        }
+        public void create_Client(string msg)
+        {
+            tcp_client_player = new TcpClient();
+            tcp_client_player.Connect("192.168.1.10", 10024);
+            client_network_stream = tcp_client_player.GetStream();
+            client_stream_writer = new StreamWriter(client_network_stream);
+           // client_stream_reader= new StreamReader(client_network_stream);
+            client_stream_writer.AutoFlush= true;
+
+            
+
+
+
+        }
+        public void Send_Massege(string msg)
+        {
+
+            try
+            {
+
+                tcp_client_player = new TcpClient();
+                tcp_client_player.Connect("192.168.1.10", 10024);
+                client_network_stream = tcp_client_player.GetStream();
+                client_stream_writer = new StreamWriter(client_network_stream);
+              //  client_stream_reader = new StreamReader(client_network_stream);
+                client_stream_writer.AutoFlush = true;
+                client_stream_writer.WriteLine(msg);
+                client_stream_writer.Close();
+            }
+           catch (Exception e)
+            {
+                tcp_client_player.Connect("192.168.1.10", 10024);
+
+                client_network_stream = tcp_client_player.GetStream();
+                client_stream_writer = new StreamWriter(client_network_stream);
+
+
+                client_stream_writer.WriteLine(msg);
+                client_stream_writer.Close();
+            }
+        }
+
+        public void open_tab()
+        {
+            Application.Run(new Form1());
+        }
+    }
+}
